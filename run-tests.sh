a4e43e63a91f2d92a42682708d9cf576673a29b6
Dan Ryan <dan@danryan.co>
2018-08-26 20:24:13 -0400

    upate test runner
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- run-tests.sh
+++ run-tests.sh
@@ -46,10 +46,10 @@ else
 fi
 
 echo "Installing dependencies…"
-PIPENV_PYTHON=2.7 pipenv run pip install -e . --upgrade
-PIPENV_PYTHON=3.7 pipenv run pip install -e . --upgrade
 PIPENV_PYTHON=2.7 pipenv install --dev
 PIPENV_PYTHON=3.7 pipenv install --dev
+PIPENV_PYTHON=2.7 pipenv run pip install -e . --upgrade
+PIPENV_PYTHON=3.7 pipenv run pip install -e . --upgrade
 
 echo "$ pipenv run time pytest -v -n auto tests -m \"$TEST_SUITE\""
 # PIPENV_PYTHON=2.7 pipenv run time pytest -v -n auto tests -m "$TEST_SUITE" | prefix 2.7 &
